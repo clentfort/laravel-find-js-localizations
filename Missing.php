f0fe2e627f318daa6aa0cf2a2b3f5bdd480c26d2
Christian Lentfort <unkieee@gmail.com>
2017-03-13 11:10:38 +0700

    Use Finder-class in POC code

--- Missing.php
+++ Missing.php
@@ -8,42 +8,7 @@ use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 use Symfony\Component\Process\Process;
 
-/**
- * @param string $directory The directory to find the files in
- * @param string $endig The filetype
- * @return Collection An array of strings
- */
-function findFiles($directory, $ending = '.js') 
-{
-  $filesystem = new Filesystem();
-  return (new Collection($filesystem->allFiles($directory)))
-    ->filter(function ($file) use ($ending) {
-      return Str::endsWith($file, $ending);
-    })
-    ->map(function ($file) use ($directory) {
-      return $directory . DIRECTORY_SEPARATOR . $file->getRelativePathname();
-    });
-}
-
-/**
- * @param Collection $files
- * @return array 
- */
-function parseJsFiles($files)
-{
-  $finder = new Process('node index.js');
-  $finder->setInput($files->implode("\n"));
-  $status = $finder->run();
-
-  $foundKeys = new Collection(json_decode($finder->getOutput()));
-
-  $errors = array_filter(explode(PHP_EOL, $finder->getErrorOutput()));
-  if (json_last_error() !== JSON_ERROR_NONE) {
-    array_push($errors, json_last_error_msg());
-  }
-  return [$foundKeys, $errors, $status];
-}
-
+use clentfort\LaravelFindJsLocalizations\Finder;
 
 /**
  * Builds a list of translations keys from a file a sourceFileKeyList
@@ -77,7 +42,7 @@ function getLanguageDirectories($directory) {
   return (new Collection($filesystem->directories($directory)));
 }
 
-list($sourceFileKeyList, $errors, $status) = parseJsFiles(findFiles('./resources/assets/js'));
+list($sourceFileKeyList, $errors) = (new Finder('./resources/assets/js', '/usr/bin/node'))->findTranslationKeys();
 $translationFileKeyMap = sourceFileKeyListToTranslationFileKeyMap($sourceFileKeyList);
 $translationFiles = $translationFileKeyMap->keys();
 $languageDirs = getLanguageDirectories('./resources/lang');
